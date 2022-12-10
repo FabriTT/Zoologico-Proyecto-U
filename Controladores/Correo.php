527c37e0c6990a6f4d8e790f32240f391a164a30
FabriTT <105324811+FabriTT@users.noreply.github.com>
2022-12-10 19:09:43 -0400

    Update Correo.php

--- Controladores/Correo.php
+++ Controladores/Correo.php
@@ -27,7 +27,7 @@
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'bioparque.vesty.pakos@gmail.com';                     //SMTP username
-        $mail->Password   = 'ifbrqzgkpzemeimg';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
